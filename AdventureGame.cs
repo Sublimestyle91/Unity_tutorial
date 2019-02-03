2fbfb3188cc1db1649d2d0cfa7603580c5572ab7
Sublimestyle91 <41523108+Sublimestyle91@users.noreply.github.com>
2019-02-04 08:45:38 +0900

    Add files via upload

---
+++ AdventureGame.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // makes it possible to use the class TEXT. We have to go to this namespace
+
+
+public class AdventureGame : MonoBehaviour
+{
+    // Variables
+    [SerializeField] Text textComponent;                // don't capitalize the variable name. The value of this is set by dragging in the textbox to it
+    [SerializeField] State startingState;               // The SerializeField makes this variable visible in the inspector
+
+
+    // classes
+    State state;   // This will change with each new state. That is why we have two State variables
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageNewState();
+    }
+
+    private void ManageNewState()
+    {
+        var nextState = state.GetNextState();
+        for(int i = 0; i < nextState.Length; i ++ )
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))     // If a number is pressed, it goes signals true
+            {
+                state = nextState[i];
+                textComponent.text = state.GetStateStory();
+            }
+        }
+        
+    }
+}
+
+
+
+
+
+
+
+ /* In GameOject, there is now a new area where we put startingoject in the inspector.
+  * This is because of the SerializeField State... variable. The starting object was the 
+ * scriptable object that was placed in this serializefield. We then set the state equal
+ * to the starting state. We then used a method to get the variable in the starting state
+ * that held the text. This variale went to the screen 
+ * which is the variale 
+ * textComponent.text
+ */
---
+++ State.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+/* This will create an asset that we can pick in the menu
+ * The asset will be a scriptable object
+ */
+public class State : ScriptableObject 
+    /* We originally had MonoBehaviour where Scriptable oject is
+     * We did this so we can create a scriptable oject for our text files
+     */
+   
+{
+   [TextArea(10,14)] [SerializeField] string storyText; // serializefield puts this variable in the inspector. Makes a variale we can edit in inspector
+   // TextArea makes the variale have a box. It will be 10 spaces big, and we will be able to scroll after 14
+   [SerializeField] State[] nextStates;
+
+    public string GetStateStory()  // These are just getter methods
+    {
+        return storyText;
+    }
+    public State[] GetNextState() // getter method to access variables
+    {
+        return nextStates;
+    }
+}
